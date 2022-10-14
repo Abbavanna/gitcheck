44b85382bf22b6cde3fbd37ea688c28ab11ee6e6
pavan <pavankumarabbavana@gmail.com>
2022-10-14 22:23:01 +0200

    Added few prints

--- test.py
+++ test.py
@@ -6,8 +6,12 @@ def check_reboot():
 
 def main():
 	print("mahaan")
+	print("whatever you want to do you do , dont tell me")
+	print(" python kuda easy ga ne undhe antha kastam em kadhu")
+	print("Vadikanna mundhu maname cheyali")
 	pass
 
 def check():
 	"""RRR"""
+
 main()
